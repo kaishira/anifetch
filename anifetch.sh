6f10698aeb35d8dc89238d02b8bf37cf4c876195
カイシラ <74321096+kaishira@users.noreply.github.com>
2025-01-07 18:04:55 -0800

    Create anifetch.sh

---
+++ anifetch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+echo ""
+
+art=$(cat << 'EOF'
+
+⢕⢕⢕⢕⢕⠅⢗⢕⠕⣠⠄⣗⢕⢕⠕⢕⢕⢕⠕⢠⣿⠐⢕⢕⢕⠑⢕⢕⠵⢕
+⢕⢕⢕⢕⠁⢜⠕⢁⣴⣿⡇⢓⢕⢵⢐⢕⢕⠕⢁⣾⢿⣧⠑⢕⢕⠄⢑⢕⠅⢕
+⢕⢕⠵⢁⠔⢁⣤⣤⣶⣶⣶⡐⣕⢽⠐⢕⠕⣡⣾⣶⣶⣶⣤⡁⢓⢕⠄⢑⢅⢑
+⠍⣧⠄⣶⣾⣿⣿⣿⣿⣿⣿⣷⣔⢕⢄⢡⣾⣿⣿⣿⣿⣿⣿⣿⣦⡑⢕⢤⠱⢐
+⢠⢕⠅⣾⣿⠋⢿⣿⣿⣿⠉⣿⣿⣷⣦⣶⣽⣿⣿⠈⣿⣿⣿⣿⠏⢹⣷⣷⡅⢐
+⣔⢕⢥⢻⣿⡀⠈⠛⠛⠁⢠⣿⣿⣿⣿⣿⣿⣿⣿⡀⠈⠛⠛⠁⠄⣼⣿⣿⡇⢔
+⢕⢕⢽⢸⢟⢟⢖⢖⢤⣶⡟⢻⣿⡿⠻⣿⣿⡟⢀⣿⣦⢤⢤⢔⢞⢿⢿⣿⠁⢕
+⢕⢕⠅⣐⢕⢕⢕⢕⢕⣿⣿⡄⠛⢀⣦⠈⠛⢁⣼⣿⢗⢕⢕⢕⢕⢕⢕⡏⣘⢕
+⢕⢕⠅⢓⣕⣕⣕⣕⣵⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣷⣕⢕⢕⢕⢕⡵⢀⢕⢕
+⢑⢕⠃⡈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢃⢕⢕⢕
+
+EOF
+)
+
+text="OS: $(uname -o)
+Hostname: $(hostname)
+User: $(whoami)
+Kernel: $(uname -r)
+Uptime: $(uptime -p)"
+
+IFS=$'\n' read -r -d '' -a info_array <<< "$text"
+IFS=$'\n' read -r -d '' -a art_array <<< "$art"
+
+max_lines=${#info_array[@]}
+if [ ${#art_array[@]} -gt $max_lines ]; then
+	    max_lines=${#art_array[@]}
+fi
+
+for ((i=0; i<max_lines; i++)); do
+	    info_line="${info_array[i]}"
+	        art_line="${art_array[i]}"
+		    printf "%-40s %s\n" "$art_line" "$info_line"
+	    done
+
+echo ""
